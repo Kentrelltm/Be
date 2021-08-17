284c3959f6ee8b9426548b8d7d07585f3cdb8714
Kentrelltm <78074550+Kentrelltm@users.noreply.github.com>
2021-08-17 06:48:26 -0400

    Minor changes

--- login.cpp
+++ login.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 string line = "----------------------------------------";
 
+
 bool IsLoggedIn() {
     string username, password, un, pw, bio;
 
@@ -22,36 +23,17 @@ bool IsLoggedIn() {
         return false;
     }
 }
-void userProfile() {
-    int pick;
-    char bio[100];
-    system("clear");
-    cout << "Hello and Welcome to your profile!!" << endl;
-    cout << line;
-    cout << "These are your options as of now:\n1. Edit Bio\nPlease select an option: ";
-    switch (pick) {
-        case 1: {
-            cout << "Enter Bio information\n >>> "; cin >> bio;
-            ofstream file;
 
-        }
-        
-            
-    
-        default: {
-        userProfile();
-        }
-    };
-
-}
 
 
 int main() {
     int choice;
+    string username, password, bio;
+    
 
     cout << "1: Register\n2: Login\nYour choice >> "; cin >> choice;
     if (choice == 1) {
-        string username, password;
+        // string username, password;
 
         cout << "Select a username: "; cin >> username;
         cout << "Select a password: "; cin >> password;
@@ -72,9 +54,28 @@ int main() {
             return 0;
         }
         else {
-            cout << "Successfully logged in!" << endl;
+            char pick;
             
-            return 1;
-        }
-    }
-}
+            system("clear");
+            cout << "Successfully logged in!" << endl;
+            cout << "Welcome to your profile!!\n\n" << endl;
+            cout << "Name: " + username << endl;
+            cout << "Edit bio?\n\n\t\t\t (Yes/No)" << endl; cin >> pick;
+            if (pick == 'y') {
+                cout << "\n\n\t >>>> "; cin >> bio;
+
+                ofstream file;
+                file.open(username + ".txt",ios::in);
+                file << bio << endl;
+                cout << bio << endl;
+                main();
+            }
+            else {
+                main();
+            }
+            
+            
+
+        };
+    };
+};
